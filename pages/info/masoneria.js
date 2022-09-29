5f44c04c0b7dbf902085efff0f55bf17233a8d06
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-09-29 18:42:40 +0200

    1 file modified

--- pages/info/masoneria.js
+++ pages/info/masoneria.js
@@ -9,23 +9,25 @@ export default function Mason() {
           <h2>¿Que es?</h2>
           <p>
             La masonería se refiere a una organización de carácter iniciático,
-            filantrópico, simbólico, filosófico, selectivo, internacional,
-            humanista y con una estructura federal, fundada en un sentimiento de
-            fraternidad. Sus orígenes se remontan a los gremios locales de
-            canteros, alabñiles y artesanos de la piedra que, desde el final del
-            siglo XIII, regulaban la cualificación de estos canteros y su
-            interacción con autoridades y clientes. La masonería acyual consiste
-            en dos grandes grupos:
+            filantrópico, simbólico, filosófico, internacional y humanista
+            fundada en un sentimiento de fraternidad. Sus orígenes se remontan a
+            los gremios locales de canteros, alabñiles y artesanos de la piedra
+            que, desde el final del siglo XIII, regulaban la cualificación de
+            estos canteros y su interacción con autoridades y clientes
           </p>
+        </article>
+        <article>
+          <h2>Dos ramas</h2>
+          <p>La masonería actual consiste en dos grandes grupos:</p>
           <ul>
             <li>
-              Masonería regular: insiste en que cada miembro crea en un ser
-              supremo llamado Gran Arquitecto del Universo, donde las mujeres no
-              están admitidas y está prohibído hablar en las logias de religion
-              o política.
+              Masonería regular anglosajona: insiste en que cada miembro crea en
+              un ser supremo llamado Gran Arquitecto del Universo (GADU) donde
+              las mujeres no están admitidas y está prohibído hablar en las
+              logias de religion o política.
             </li>
             <li>
-              Masonería simbólica o continental: remueve ciertas o todas las
+              Masonería regular continental: remueve ciertas o todas las
               restricciones de la masonería regular
             </li>
           </ul>
@@ -34,14 +36,15 @@ export default function Mason() {
           <h2>Objetivo</h2>
           <ul>
             <li>
-              <h3>Integridad</h3>
+              <h4>Integridad</h4>
               <p>
                 Construir buena gente: los miembros están unidos a través de una
-                comprensión de la unidad y la equidad
+                comprensión de la unidad y la equidad y deben contar con una
+                alta moral.
               </p>
             </li>
             <li>
-              <h3>Akmistad</h3>
+              <h4>Amistad</h4>
               <p>
                 Construyendo juntos: amistad entre miembros que comparten un
                 sentido de unión que fortalece su capacidad para tener éxito y
@@ -49,14 +52,14 @@ export default function Mason() {
               </p>
             </li>
             <li>
-              <h3>Respeto</h3>
+              <h4>Respeto</h4>
               <p>
                 Construyendo unidad: la masonería junta personas sin distinguir
-                de su raza,religión o política{' '}
+                de su raza,religión o bando político.
               </p>
             </li>
             <li>
-              <h3>Caridad</h3>
+              <h4>Caridad</h4>
               <p>
                 Construyendo compasión: la bondad y las donaciones caritativas
                 están profundamente arraigadas en los principios de la masonería
@@ -65,7 +68,7 @@ export default function Mason() {
                 diversas causas a través de eventos de recaudación de fondos o
                 trabajo voluntario. Las personas pueden hacer una contribución
                 importante a nivel local, nacional y global al dar su tiempo y
-                dinero
+                dinero.
               </p>
             </li>
           </ul>
@@ -82,7 +85,7 @@ export default function Mason() {
           </p>
           <ul>
             <li>
-              <h3>Aprendiz</h3>
+              <h4>Aprendiz</h4>
               <p>
                 La primera experiencia del candidato con las ceremonias de la
                 logia. Una vez complete el rito de iniciación se le otorga el
@@ -91,7 +94,7 @@ export default function Mason() {
               </p>
             </li>
             <li>
-              <h3>Compañero</h3>
+              <h4>Compañero</h4>
               <p>
                 El segundo grado expone al hermano a más simbolismo y filosofía
                 de la hermandad donde el masón se dedica a aprender y ve cómo el
@@ -99,13 +102,128 @@ export default function Mason() {
               </p>
             </li>
             <li>
-              <h3>Maestro</h3>
+              <h4>Maestro</h4>
               <p>
                 Esto convierte al masón en miembro completo de la fraternidad,
                 disfrutando los derechos y responsabilidades de ser miembro.El
                 masón es enfrentado con la realidad de la muerte, se enfrenta
                 con la inmortalidad del alma y la vida eterna.
               </p>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h2>Características de un masón</h2>
+          <p>
+            Toda persona que esté interesada en ser masón tiene que tener los
+            siguientes tres requisitos:
+          </p>
+          <ul>
+            <li>
+              Ser una persona <b>libre</b>, puesto que la solicitud de
+              iniciación a la Masonería ha de ser consecuencia de la innata
+              libertad individual, sin ninguna presión, influencia, dependencia,
+              o circunstancia que la coarte.
+            </li>
+            <li>
+              Ser una persona de <b>buenas costumbres</b>, es decir, digna de
+              confianza, honrada en su vida privada, en su forma de trabajar, y
+              de buena reputación,
+            </li>
+            <li>
+              creer en un
+              <b> prncipio superior o causa primera de todo lo creado </b>, al
+              que se denomina comúnmente como Dios, y que los masones,
+              respetando siempre la libertad del individuo ante cualquier
+              creencia particular, nombran como Gran Arquitecto del Universo
+              (GADU).
+            </li>
+          </ul>
+          <p>
+            Además, para ser miembro en España es preciso ser varón, mayor de 21
+            años, sin distinción de nacionalidad, grupo social o religión, tener
+            una mínima capacidad de comprensión de los conceptos filosóficos de
+            la Orden y disponer de una “ciencia, arte, oficio, o renta” tal como
+            expresan los antiguos lineamientos de la Orden.
+          </p>
+        </article>
+        <article>
+          <h2>¿Secreto?</h2>
+          <p>
+            Asociación universalmente extendida, originariamente secreta, cuyos
+            miembros forman una hermandad iniciática y jerarquizada, organizada
+            en logias, de ideología racionalista y carácter filantrópico.
+            <br />
+            Masonería por la RAE (Real Academia Española)
+          </p>
+          <p>
+            La masonería no es una sociedad secreta, sino discreta y con algunos
+            secretos que son revelados a sus miembros a medida que estos
+            progresan. En lo que se refiere a las actividades de las logias,
+            estas son discretas. Con respecto al secreto, existen dos tipos de
+            secretos prioritarios, uno de ellos asociado con el reconocimiento,
+            las palabras de pase, los toques al saludarse y las respuestas a
+            preguntas específicas para poder ingresar a la orden.
+          </p>
+          <p>
+            Según algunos, lo más indicado sería denominar &quot;sociedades
+            discretas&quot; a las instituciones más famosas (masonería,​
+            rosacruz, Iluminados de Baviera, martinismo, etc.), ya que las
+            sociedades secretas solo serían frecuentadas y conocidas por sus
+            miembros, que a diferencia de las discretas son de público
+            conocimiento, manifiesto en sus simbologías edilicias, tenidas
+            blancas, conferencias abiertas, proselitismo, y todo tipo de
+            publicaciones e informaciones. Pero, pese a ello, son más conocidas
+            como sociedades secretas.
+            <br />
+            Se sabe hoy día, sin embargo, que muchas de estas sociedades
+            divulgan a sus socios de los más bajos niveles información sesgada o
+            parcial de sus actividades para proteger los más altos secretos, que
+            solo son revelados a los miembros de los niveles más altos. Asimismo
+            muchas de las conferencias públicas se realizan para divulgar una
+            imagen de la sociedad que conviene a esta. En realidad solo unos
+            pocos escogidos dentro de estas sociedades saben realmente qué están
+            haciendo.
+          </p>
+        </article>
+        <article>
+          <h2>Mal vista</h2>
+          <p>
+            Desde su fundación, la masonería ha encontrado la oposición de
+            distintos tipos de actores sociales. Los motivos de esta oposición
+            pueden referirse a la institución masónica en cuanto forma de
+            organización, o bien poner el acento en una característica
+            pretendidamente negativa de sus principios filosóficos y valores
+            morales.
+          </p>
+          <ul>
+            <li>
+              La Iglesia católica ha condenado sistemáticamente la filiación a
+              la masonería en innumerables documentos, y ha decretado que es
+              incompatible, por sus principios, con la doctrina y la fe de la
+              Iglesia católica.
+            </li>
+            <li>
+              El integrismo islámico: por ejemplo, la masonería está prohibida
+              en Irán y en muchos países islámicos.
+            </li>
+            <li>
+              Monarquías absolutistas: el zar Alejandro I, los monarcas
+              españoles Felipe V, Fernando VI, Carlos III y Fernando VII.
+            </li>
+            <li>
+              Las dictaduras fascistas (Adolf Hitler, Mussolini, Francisco
+              Franco, Miguel Primo de Rivera y Salazar) del siglo XX rechazaron
+              y atacaron a la masonería, porque la consideraban reaccionaria y
+              contraria a los intereses nacionales. En el fascismo italiano
+              inicialmente participaron muchos masones.
+            </li>
+            <li>
+              Las dictaduras socialistas: la Unión Soviética (Tercer Congreso de
+              la Internacional Socialista 1921). Una excepción a este conjunto
+              es Cuba, que no ilegalizó la francmasonería debido a que el héroe
+              nacional, José Martí, era masón, aunque esta afirmación es
+              discutida por muchos y no se enseña oficialmente en ese país.
             </li>
           </ul>
         </article>
@@ -121,7 +239,7 @@ export default function Mason() {
             >
               Wikipedia(en) FreeMasonry
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a
               href="https://es.wikipedia.org/wiki/Francmasoner%C3%ADa"
@@ -130,7 +248,7 @@ export default function Mason() {
             >
               Wikipedia(es) Francmasonería
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a
               href="https://www.ugle.org.uk/discover-freemasonry/what-is-freemasonry"
@@ -139,7 +257,7 @@ export default function Mason() {
             >
               Ugle
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a
               href="https://www.freemason.com/what-is-freemasonry/"
@@ -148,27 +266,43 @@ export default function Mason() {
             >
               FreeMason
             </a>
-          </li>{' '}
+          </li>
           <li>
-            <a href="" target="_BLANK" rel="noopener nofollow noreferrer">
-              text
+            <a
+              href="http://www.dunnsrockmasoniclodge.org/aims_and_objectives_of_freemason.htm"
+              target="_BLANK"
+              rel="noopener nofollow noreferrer"
+            >
+              DunnsRock Masonic Lodge
             </a>
-          </li>{' '}
+          </li>
           <li>
-            <a href="" target="_BLANK" rel="noopener nofollow noreferrer">
-              text
+            <a
+              href="https://gle.org/masones/caracteristicas/"
+              target="_BLANK"
+              rel="noopener nofollow noreferrer"
+            >
+              Gle
             </a>
-          </li>{' '}
+          </li>
           <li>
-            <a href="" target="_BLANK" rel="noopener nofollow noreferrer">
-              text
+            <a
+              href="https://dle.rae.es/masoner%C3%ADa"
+              target="_BLANK"
+              rel="noopener nofollow noreferrer"
+            >
+              RAE Masonería
             </a>
-          </li>{' '}
+          </li>
           <li>
-            <a href="" target="_BLANK" rel="noopener nofollow noreferrer">
-              text
+            <a
+              href="https://es.wikipedia.org/wiki/Sociedad_secreta"
+              target="_BLANK"
+              rel="noopener nofollow noreferrer"
+            >
+              Wikipedia(es) Sociedad Secreta
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a href="" target="_BLANK" rel="noopener nofollow noreferrer">
               text
